728fa225964d866fb9d0f61596d407d613d1a3ef
tommasomannori-aivirth <aivirth.dev@gmail.com>
2023-04-23 20:58:11 +0200

    Added pdf embeding to cliente summary

--- config/filesystems.php
+++ config/filesystems.php
@@ -71,6 +71,7 @@ return [
 
     'links' => [
         public_path('storage') => storage_path('app/public'),
+        public_path('visure_camerali') => storage_path('app/visure_camerali')
     ],
 
 ];
