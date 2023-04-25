d42d5501fd015df3723272a758e22ca3e03acf4b
tommasomannori-aivirth <aivirth.dev@gmail.com>
2023-04-25 15:01:46 +0200

    Added route auth guards

--- routes/web.php
+++ routes/web.php
@@ -23,17 +23,17 @@ Route::get('/', function(){
     return view('welcome', ['pageTitle' => 'Home']);
 })->name('home');
 
-Route::get('/clienti', [ClienteController::class, 'index'])->name('all_clienti');
+Route::get('/clienti', [ClienteController::class, 'index'])->name('all_clienti')->middleware('auth');
 
-Route::get('/cliente/create', [ClienteController::class, 'create'])->name('crea_utente');
+Route::get('/cliente/create', [ClienteController::class, 'create'])->name('crea_utente')->middleware('auth');
 Route::post('/cliente/add', [ClienteController::class, 'store'])->name('store_cliente');
 
-Route::get('/cliente/{id}/edit', [ClienteController::class, 'edit'])->name('edit_cliente');
+Route::get('/cliente/{id}/edit', [ClienteController::class, 'edit'])->name('edit_cliente')->middleware('auth');
 Route::put('/cliente/{id}/update', [ClienteController::class, 'update'])->name('update_cliente');
 Route::delete('/cliente/{id}/delete', [ClienteController::class, 'destroy'])->name('delete_cliente');
-Route::get('/cliente/{id}', [ClienteController::class, 'show'])->name('summary_cliente');
+Route::get('/cliente/{id}', [ClienteController::class, 'show'])->name('summary_cliente')->middleware('auth');
 
 
-Route::get('/login', [UserController::class, 'login'])->name('login');
-Route::get('/logout', [UserController::class, 'logout'])->name('logout');
+Route::get('/login', [UserController::class, 'login'])->name('login')->middleware('guest');
+Route::get('/logout', [UserController::class, 'logout'])->name('logout')->middleware('auth');
 Route::post('/user/authenticate', [UserController::class, 'authenticate'])->name('authenticate');
