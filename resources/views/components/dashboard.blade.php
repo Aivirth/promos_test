2e4d60701b91dbdb85edd7637876bef3a3548aa4
tommasomannori-aivirth <aivirth.dev@gmail.com>
2023-04-26 14:03:58 +0200

    Added sidebar toggle script

--- resources/views/components/dashboard.blade.php
+++ resources/views/components/dashboard.blade.php
@@ -1,41 +1,23 @@
 <x-base>
- 
-        {{-- <div class="">
-            @if (Route::has('login'))
-                <div class="">
-                    @auth
-                        <a href="{{ url('/home') }}" class="">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="">Log in</a>
-    
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-        </div> --}}
-    
-        {{-- <div>
-            @php
-                echo auth()->user();
-                echo auth()->user()->isAdmin();
-            @endphp
-        </div> --}}
-    
-        <div class="d-flex" id="wrapper">
-            @include('../partials._sidebar')
-            <div id="page-content-wrapper">
-                @include('../partials._topnav')
-                <div class="container-fluid px-4">
+    <div class="d-flex" id="wrapper">
+        @include('../partials._sidebar')
+        <div id="page-content-wrapper">
+            @include('../partials._topnav')
+            <div class="container-fluid px-4">
                 {{ $slot }}
-                </div>
             </div>
         </div>
+    </div>
 
+    <script>
+        const wrapper = document.getElementById("wrapper");
+        const toggleButton = document.getElementById("menu-toggle");
+
+        if(wrapper && toggleButton){
+            toggleButton.onclick = function () {
+                wrapper.classList.toggle("toggled");
+            };
+        }
+    </script>
 
 </x-base>
-
-
-
-
